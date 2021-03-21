6b3b6290e13026defce4816bdee10d63b1fa1968
Kipras <kipras@anaweb.lt>
2021-03-21 20:54:28 +0200

    Created Article Search controller with URL input

---
+++ views/articleSearch.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+
+    <title>15min.lt crawler</title>
+</head>
+
+<body>
+    <form method="post" action="article/submit">
+        <input type="text" name="url">
+        <button type="submit">OK</button>
+    </form>
+</body>
+</html>
+<?php
+
