d718d2876f45de0298c6ba143aabb6080124c1c0
Kipras <kipras@anaweb.lt>
2021-03-21 20:50:45 +0200

    Public/index.php calling Core class

--- public/index.php
+++ public/index.php
@@ -2,15 +2,6 @@
 
 use Crawler\Core;
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
 $init = new Core;
-
-echo "So far - so good \n";
-//use Crawler\Crawler;
-//
-//$class = new Crawler();
-//$class->setContent();
-//print_r($class->content);
-
-
