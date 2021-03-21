0b002bd61ce354e044d065275003e80b75ed1b1d
Kipras <kipras@anaweb.lt>
2021-03-21 20:19:25 +0200

    Updated .gitignore

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-
-use Crawler\Crawler;
-
-$class = new Crawler();
-echo $class->getResponse();
---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use Crawler\Core;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$init = new Core;
+
+echo "So far - so good \n";
+//use Crawler\Crawler;
+//
+//$class = new Crawler();
+//$class->setContent();
+//print_r($class->content);
+
+
